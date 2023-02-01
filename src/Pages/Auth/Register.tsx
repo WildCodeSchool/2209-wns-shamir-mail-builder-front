07595e59d8361ac5c5340c23b3c4cfb750028cc0
DieumegardGregory <dieumegard.gregory@gmail.com>
2023-02-01 09:39:57 +0100

    ajout du module création compte, continuation sur l'authentification

--- src/Pages/Auth/Register.tsx
+++ src/Pages/Auth/Register.tsx
@@ -1,16 +1,45 @@
-import { Container, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Container, Typography, CircularProgress } from '@mui/material';
 import { gql, useMutation } from '@apollo/client';
 import { ContentStyle } from '../../layouts/Main/UserLayoutConfig';
 import RegisterForm from '../../Components/RegisterForm/RegisterForm';
 
+export const CREATE_ACCOUNT = gql`
+mutation CreateUser($username: String!, $password: String!, $email: String!, $phone: String!){
+  createUser(username: $username, password: $password, email: $email, phone: $phone) {
+    username
+    phone
+    email
+  }
+}`;
+
 export default function Register() {
+  const navigate = useNavigate();
+  const [signUp, { loading, error }] = useMutation(CREATE_ACCOUNT, {
+    onCompleted: () => {
+      navigate('/auth/login');
+    },
+  });
+  if (loading) return <CircularProgress />;
+  if (error) {
+    return (
+      <Typography>
+        Error !
+      </Typography>
+    );
+  }
+
+  const handleSignUp = (username: string, password: string, email: string, phone: string) => {
+    signUp({ variables: { username, password, email, phone } });
+  };
+
   return (
     <ContentStyle>
       <Container maxWidth="lg">
         <Typography variant="h4" component="h1" gutterBottom>
           Création de compte
         </Typography>
-        <RegisterForm />
+        <RegisterForm handleSignUp={handleSignUp} />
       </Container>
     </ContentStyle>
   );
