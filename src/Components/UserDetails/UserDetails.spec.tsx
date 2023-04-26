25a14e1034a390c657c3f5bf9f3c3d672c5916ba
Dieumegard Grégory <85200406+DieumegardGregory@users.noreply.github.com>
2023-04-26 16:26:01 +0200

    [ADD] test sur composant UserDetails

---
+++ src/Components/UserDetails/UserDetails.spec.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { formatPhoneNumber } from '../../helpers';
+import UserDetails from './UserDetails';
+
+const userInfos = {
+  username: 'Joël Miller',
+  createdAt: new Date(),
+  email: 'joel.miller@gmail.com',
+  phone: formatPhoneNumber('0123456789'),
+};
+
+const handleModifyAccount = jest.fn();
+
+describe('UserDetails view', () => {
+  it('should render the details of a user', () => {
+    render(<UserDetails userInfos={userInfos} handleModifyAccount={handleModifyAccount} />);
+    expect(screen.getByText(/Joël Miller/i)).toBeInTheDocument();
+    expect(screen.getByText(/joel\.miller@gmail\.com/i)).toBeInTheDocument();
+    expect(screen.getByText(/01\.23\.45\.67\.89/i)).toBeInTheDocument();
+  });
+});
